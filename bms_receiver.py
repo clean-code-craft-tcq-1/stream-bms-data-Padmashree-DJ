8c665faefe9d6d8094c9e7f2d5074247f1322d53
Ravi Kudumu <ravikudumu@gmail.com>
2021-06-20 10:45:14 +0530

    Add files via upload

--- bms_receiver.py
+++ bms_receiver.py
@@ -1,50 +1,24 @@
-
+import time,datetime
+import  bms_receiver_utility as bms_utility
 def recevie_bms_details():
     final_bms_data = []
     while(True):
         bms_details = input()
-        bms_details = format_bms_data(bms_details)
+        bms_details = bms_utility.format_bms_data(bms_details)
         final_bms_data.append(bms_details)
         if len(final_bms_data) >= 5:
-            calculate_bms_statistics(final_bms_data)
+            final_display_list = bms_utility.calculate_bms_statistics(final_bms_data)
+            display_bms_statistics(final_display_list)
+        time.sleep(1 / 3)
 
-def calculate_bms_statistics(bms_data):
-    temperature_values = []
-    soc_values = []
-    final_display_list = {}
 
-    for i in range(-5, 0, 1):
-        temperature_values.append(float(bms_data[i][0][1]))
-        soc_values.append(float(bms_data[i][1][1]))
-
-    soc_values.sort()
-    temperature_values.sort()
-    final_display_list['temperature'] = temperature_values
-    final_display_list['soc'] = soc_values
-    display_bms_statistics(final_display_list)
-    
 def display_bms_statistics(bms_data):
+    print('BMS Parameter details:',datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
     print('Max Temperature:', bms_data['temperature'][-1])
     print('Min Temperature:', bms_data['temperature'][0])
-    print('Average Temperature:', average(bms_data['temperature']))
+    print('Average Temperature:', bms_utility.average(bms_data['temperature']))
     print('Max SOC:', bms_data['soc'][-1])
     print('Min SOC:', bms_data['soc'][0])
-    print('Average SOC:', average(bms_data['soc']))
-
-
-
-def format_bms_data(bms_details):
-    bms_details = bms_details.replace('Temp','Temperature')
-    bms_details = bms_details.replace('and', ',')
-    bms_details = bms_details.replace('value is', '=')
-    bms_details = bms_details.replace(' ', '')
-    bms_details = bms_details.split(',')
-    bms_details[0] = bms_details[0].split('=')
-    bms_details[1] = bms_details[1].split('=')
-    return bms_details
-
-def average(list):
-    return round(sum(list) / len(list), 2)
-
+    print('Average SOC:', bms_utility.average(bms_data['soc']), '\n')
 
 recevie_bms_details()
