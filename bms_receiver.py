bc1e1e4ec5fce41f32ffb077fa553f7a11d999cc
Ravi Kudumu <ravikudumu@gmail.com>
2021-06-21 17:31:03 +0530

    Update bms_receiver.py

--- bms_receiver.py
+++ bms_receiver.py
@@ -4,25 +4,23 @@ def recevie_bms_details():
     final_bms_data = []
     while(True):
         bms_details = input()
-        if bms_details == '':
+        if (bms_details) == '':
             break
-        print(bms_details)
         bms_details = bms_utility.format_bms_data(bms_details)
-        
-#         final_bms_data.append(bms_details)
-#         if len(final_bms_data) >= 5:
-#             final_display_list = bms_utility.calculate_bms_statistics(final_bms_data)
-#             display_bms_statistics(final_display_list)
-#         time.sleep(1 / 3)
+        final_bms_data.append(bms_details)
+        final_display_list = bms_utility.calculate_bms_statistics(final_bms_data)
+        display_bms_statistics(final_display_list)
+        time.sleep(1 / 3)
 
 
 def display_bms_statistics(bms_data):
-    print('BMS Parameter details:',datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
-    print('Max Temperature:', bms_data['temperature'][-1])
-    print('Min Temperature:', bms_data['temperature'][0])
-    print('Average Temperature:', bms_utility.average(bms_data['temperature']))
-    print('Max SOC:', bms_data['soc'][-1])
-    print('Min SOC:', bms_data['soc'][0])
-    print('Average SOC:', bms_utility.average(bms_data['soc']), '\n')
+    if (bms_data):
+        print('BMS Parameter details:',datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+        print('Max Temperature:', bms_data['temperature'][-1])
+        print('Min Temperature:', bms_data['temperature'][0])
+        print('Average Temperature:', bms_utility.average(bms_data['temperature']))
+        print('Max SOC:', bms_data['soc'][-1])
+        print('Min SOC:', bms_data['soc'][0])
+        print('Average SOC:', bms_utility.average(bms_data['soc']), '\n')
 
 recevie_bms_details()
